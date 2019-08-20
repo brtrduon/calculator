c65a8d27f21a3529103dca8a9f2b5673e07582a4
brtrduon <brianduongt@gmail.com>
2019-08-20 13:12:04 -0700

    switched to bootstrap instead of semantic-ui

--- src/App.js
+++ src/App.js
@@ -223,25 +223,32 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className='container'>
         <div id='display'>
           {this.state.display}
         </div>
+        <div className='row'>
+          <button id='seven' onClick={this.click} value={7}>7</button>
+          <button id='eight' onClick={this.click} value={8}>8</button>
+          <button id='nine' onClick={this.click} value={9}>9</button>
+          <button id='multiply' onClick={this.operand} value='*'>*</button>
+        </div>
+        <div className='row'>
+          <button id='four' onClick={this.click} value={4}>4</button>
+          <button id='five' onClick={this.click} value={5}>5</button>
+          <button id='six' onClick={this.click} value={6}>6</button>
+          <button id='subtract' onClick={this.operand} value='-'>-</button>
+        </div>
+        <div className='row'>
+          <button id='one' onClick={this.click} value={1}>1</button>
+          <button id='two' onClick={this.click} value={2}>2</button>
+          <button id='three' onClick={this.click} value={3}>3</button>
+          <button id='add' onClick={this.operand} value='+'>+</button>
+        </div>
+        
         <button id='clear' onClick={this.clear}>CE</button>
         <button id='divide' onClick={this.operand} value='/'>/</button>
-        <button id='multiply' onClick={this.operand} value='*'>*</button>
-        <button id='subtract' onClick={this.operand} value='-'>-</button>
-        <button id='add' onClick={this.operand} value='+'>+</button>
         <button id='equals' onClick={this.equals}>=</button>
-        <button id='one' onClick={this.click} value={1}>1</button>
-        <button id='two' onClick={this.click} value={2}>2</button>
-        <button id='three' onClick={this.click} value={3}>3</button>
-        <button id='four' onClick={this.click} value={4}>4</button>
-        <button id='five' onClick={this.click} value={5}>5</button>
-        <button id='six' onClick={this.click} value={6}>6</button>
-        <button id='seven' onClick={this.click} value={7}>7</button>
-        <button id='eight' onClick={this.click} value={8}>8</button>
-        <button id='nine' onClick={this.click} value={9}>9</button>
         <button id='zero' onClick={this.click} value={0}>0</button>
         <button id='decimal' onClick={this.click} value='.'>.</button>
       </div>
